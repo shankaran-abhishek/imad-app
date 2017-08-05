4cfdee93615a1cde1e3a024d4956dde3596b481e
shankaran-abhishek <30748812+shankaran-abhishek@users.noreply.github.com>
2017-08-05 16:53:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article 1', function (req,res){
+app.get('/article-1', function (req,res){
     res.send("Article 1 requested!!!!");
 });
 
-app.get('/article 2', function (req,res){
+app.get('/article-2', function (req,res){
     res.send("Article 2 requested!!!!");
 });
 
-app.get('/article 3', function (req,res){
+app.get('/article-3', function (req,res){
     res.send("Article 3 requested!!!!");
 });
 app.get('/ui/style.css', function (req, res) {
